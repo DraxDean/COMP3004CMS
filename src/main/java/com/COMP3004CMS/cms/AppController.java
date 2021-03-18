eeb1bcb60f960ce242565b9d070f5fc9be4532ae
Wenyu Zhang <ivoryzhangwy@gmail.com>
2021-03-18 15:13:31 -0400

    minor

--- src/main/java/com/COMP3004CMS/cms/AppController.java
+++ src/main/java/com/COMP3004CMS/cms/AppController.java
@@ -1,20 +1,13 @@
 package com.COMP3004CMS.cms;
 
 import com.COMP3004CMS.cms.Model.User;
-import com.COMP3004CMS.cms.Repository.UserRepository;
 import com.COMP3004CMS.cms.Service.UserDetailServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashSet;
 
 @Controller
 //@RequestMapping
@@ -28,23 +21,24 @@ public class AppController {
     PasswordEncoder encoder;
 
     @GetMapping("")
-    public String homePage(){
+    public String homePage() {
         //userRepository.save(new User("ivoryzhang", "123456"));
         return "home";
     }
 
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin() {
         return "login";
     }
+
     @GetMapping("/signup")
-    public String getSignup(){
+    public String getSignup() {
         User user = new User();
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String getSignup(User user, BindingResult bindingResult){
+    public String getSignup(User user, BindingResult bindingResult) {
         //User user = new User("ivoryzhang", encoder.encode("123456"));
         //userRepository.save(user);
         User userExists = userDetailServiceImp.findByUsername(user.getUsername());
@@ -58,13 +52,12 @@ public class AppController {
         } else {
             userDetailServiceImp.saveUser(user);
             return "redirect:/login";
-
         }
     }
 
 
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard() {
         return "dashboard";
     }
 
