423ff5281f21e3082efcbe87ce0ef0468b22283f
l34lo1 <badigiroux@gmail.com>
2021-03-26 14:51:22 -0400

    Badi giroux - create DB interface with query and result objects

---
+++ src/main/java/com/COMP3004CMS/cms/utility/db_Connection/DbQuery.java
@@ -0,0 +1,6 @@
+package com.COMP3004CMS.cms.utility.db_Connection;
+
+public class DbQuery {
+    String command;
+    String input;
+}
---
+++ src/main/java/com/COMP3004CMS/cms/utility/db_Connection/DbResult.java
@@ -0,0 +1,10 @@
+package com.COMP3004CMS.cms.utility.db_Connection;
+
+/**
+ * Result from database query, will add more items once we know what is required
+ * @param <E> Element that is required
+ */
+
+public class DbResult<E> {
+   E result;
+}
