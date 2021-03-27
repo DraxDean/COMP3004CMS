2064f3e26a0a889c63580c5b853a26608541c585
l34lo1 <badigiroux@gmail.com>
2021-03-26 21:19:17 -0400

    Badi giroux - added fragments for thymeleaf pages + configured custom layout dialect for template page types

--- src/main/java/com/COMP3004CMS/cms/WebConfig/SpringWebConfig.java
+++ src/main/java/com/COMP3004CMS/cms/WebConfig/SpringWebConfig.java
@@ -1,14 +1,23 @@
 package com.COMP3004CMS.cms.WebConfig;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
@@ -31,10 +40,49 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter implements Applicat
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-       // registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-       // registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-       // registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+       registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
+
+    /**
+     * Indicate override default template resolver and location of the view pages in CMS
+     * @return
+     */
+    @Bean
+    @Description("Thymeleaf Template Resolver")
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    /**
+     * Configuration for template engine
+     * @return
+     */
+    @Description("Thymeleaf Template Engine")
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        //templateEngine.setTemplateEngineMessageSource(messageSource());
+        return templateEngine;
+    }
+
+    /**
+     * Mapping names from controller to view objects
+     * @return
+     */
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+
+
 
 
 }
