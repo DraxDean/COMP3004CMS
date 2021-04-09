9758adcd80c89ec91f306f9c4d3e048d48540f37
l34lo1 <badigiroux@gmail.com>
2021-04-08 21:58:01 -0400

    Badi Giroux - Fixed persistance with all fields (not working) page to call SDC report generator

--- src/main/java/com/COMP3004CMS/cms/Controller/ReportController.java
+++ src/main/java/com/COMP3004CMS/cms/Controller/ReportController.java
@@ -15,41 +15,29 @@ public class ReportController {
 
     @GetMapping("/reports")
     public String displayReports(Model model){
-        model.addAttribute("gender", "ALL");
-        model.addAttribute("skip", false);
-        model.addAttribute("avg", false);
-
+        ReportRequest req = new ReportRequest(0, 99, false, false, "All");
+        model.addAttribute("req", req);
         return "reports";
     }
 
     @PostMapping ("/reports")
     public String getReports(
             ReportRequest req,
-            /*@RequestParam(value = "gender", required = false) String gender,
-            @RequestParam(value = "skip", required = false) boolean skip,
-            @RequestParam(value = "avg", required = false) boolean avg,
-            @RequestParam(value = "minGrade", required = false) double minGrade,
-            @RequestParam(value = "minGrade", required = false) double maxGrade,
-
-             */
             BindingResult bindingResult,
-            Model model)
-
-        {
+            Model model) {
         try{
+            //send back last request
+            model.addAttribute("req",req);
             System.out.println(req.skip);
             //check the gender type
            if (req.gender.equals("MALE") ){
                 System.out.println("Male Selected");
-                model.addAttribute("gender", "MALE");
             }
             if (req.skip){
                 System.out.println("Skip Selected");
-                model.addAttribute("skip", true);
             }
             if(req.avg){
                 System.out.println("averae selected");
-                model.addAttribute("avg", true);
             }
 
 
